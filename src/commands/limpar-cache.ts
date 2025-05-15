17c2878989c767b4d1099d29a9486cd193009680
Marcos <marcosviniciusreis260@gmail.com>
2025-05-15 20:45:10 -0300

    feat: cria comando de limpar cache

---
+++ src/commands/limpar-cache.ts
@@ -0,0 +1,16 @@
+import { exec } from 'child_process'
+
+export function limparCache(
+  callback: (error: Error | null, output?: string) => void,
+) {
+  exec(
+    'sudo sync && sudo sysctl -w vm.drop_caches=3',
+    (error, stdout, stderr) => {
+      if (error) {
+        callback(error, stderr)
+        return
+      }
+      callback(null, 'Cache da memória RAM limpo com sucesso')
+    },
+  )
+}
